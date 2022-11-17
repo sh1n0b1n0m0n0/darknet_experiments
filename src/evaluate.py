bbc1e4d9ab2169dc412681ea88a69e29659e4f87
romanv <romanv@zamir.co.il>
2022-11-17 07:58:32 -0500

    dvc repro has finished

--- src/evaluate.py
+++ src/evaluate.py
@@ -8,16 +8,22 @@ def main():
     ODM_path = str(Path.home()) + "/Object-Detection-Metrics/"
     pascalvoc_path = "pascalvoc.py"
     params = yaml.safe_load(open(Path.cwd() / Path("params.yaml")))["evaluate"]
-    Path(str(Path.home()) + "/darknet_experiments/" + params["save_path"]).mkdir(parents=True, exist_ok=True)
+    cwd = Path().resolve()
+
+    (cwd / params["save_path"]).mkdir(parents=True, exist_ok=True)
+
     os.chdir(ODM_path)
-    subprocess.run(["python",
-                    pascalvoc_path,
-                    "-gt", 
-                    str(Path.home()) + "/darknet_experiments/" + params["gt_path"], 
-                    "-det", 
-                    str(Path.home()) + "/darknet_experiments/" + params["det_path"], 
-                    "-sp", 
-                    str(Path.home()) + "/darknet_experiments/" + params["save_path"]])
+    subprocess.run([
+        "python",
+        pascalvoc_path,
+        "-gt",
+        str(cwd / params["gt_path"]),
+        "-det",
+        str(cwd /  params["det_path"]),
+        "-sp",
+        str(cwd /  params["save_path"]),
+        "--noplot"
+    ])
 
 if __name__ == "__main__":
     main()
