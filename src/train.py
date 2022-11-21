62366e5876fa7e0d3dd66386318081df7c2f3b6b
romanv <romanv@zamir.co.il>
2022-11-21 10:33:22 -0500

    dvc experrminet with 416x256

--- src/train.py
+++ src/train.py
@@ -12,13 +12,28 @@ def main():
     darknet_path = os.environ["DN_BIN"] + "/darknet"
 
     paths = yaml.safe_load(open(Path.cwd() / Path("paths.yaml")))["train"]
+
     subprocess_params = [darknet_path,
-                    "detector",
-                    "train",
-                    "-dont_show",
-                    "-map",
-                    paths['obj_data'],
-                    paths['cfg']]
+        "detector",
+        "train",
+        "-dont_show",
+        "-map",
+        paths['obj_data'],
+        paths['cfg']
+    ]
+
+    existing_weights = list(Path("runs/train/backup/").glob("*.weights"))
+
+    if len(existing_weights):
+        last_weights = None
+        if "runs/train/backup/yolo_best.weights" in existing_weights:
+            last_weights = "runs/train/backup/yolo_best.weights"
+        elif "runs/train/backup/yolo_last.weights" in existing_weights:
+            last_weights = "runs/train/backup/yolo_last.weights"
+        else:
+            last_weights = sorted(list(map(lambda x: str(x), existing_weights)))[-1]
+        subprocess_params.append(last_weights)
+
     print(f"train.py: running {' '.join(subprocess_params)}")
     subprocess.run(subprocess_params)
 
